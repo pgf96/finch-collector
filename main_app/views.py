4a7e04a6c9721202c859113ff2da83fc5762a433
pgfeliciano <pgfxx1@gmail.com>
2023-01-13 16:32:04 -0500

    add views for main

---
+++ main_app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Finch
+# Create your views here.
+
+finches = 'hi'
+
+
+def home(request):
+    return HttpResponse('this is home')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def finches_index(request):
+    finches = Finch.objects.all()
+    return render(request, 'finches/index.html', {'finches': finches})
+
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {'finch': finch})
